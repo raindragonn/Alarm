7bf179191864683b7628f51e12951195009d34c9
raindragonn <raindragonn@gmail.com>
2023-11-06 05:21:46 +0900

    feat: targetSdk 34로 변경

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,12 +9,12 @@ plugins {
 
 android {
     namespace = "com.bluepig.alarm"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.bluepig.alarm"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
