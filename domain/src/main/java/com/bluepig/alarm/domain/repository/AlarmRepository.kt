a8a2dc7db413a14cb9509bd2a50d5629ae57b0de
raindragonn <raindragonn@gmail.com>
2023-11-09 23:45:38 +0900

    feat: added coroutines library in domain module

--- domain/src/main/java/com/bluepig/alarm/domain/repository/AlarmRepository.kt
+++ domain/src/main/java/com/bluepig/alarm/domain/repository/AlarmRepository.kt
@@ -1,10 +1,11 @@
 package com.bluepig.alarm.domain.repository
 
 import com.bluepig.alarm.domain.entity.alarm.Alarm
+import kotlinx.coroutines.flow.Flow
 
 interface AlarmRepository {
-    suspend fun getAllAlarms(): List<Alarm>
-    suspend fun insertAlarm(alarm: Alarm) : Long
+    suspend fun getAllAlarms(): Flow<List<Alarm>>
+    suspend fun insertAlarm(alarm: Alarm): Long
     suspend fun updateAlarm(alarm: Alarm)
     suspend fun getById(id: Long): Alarm?
     suspend fun deleteById(id: Long)
