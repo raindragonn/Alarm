8bb157c49f84d4db5f5a423d4f7ff4e81e221651
raindragonn <raindragonn@gmail.com>
2023-11-25 21:58:52 +0900

    fix: 시간순으로만 정렬하도록 변경

--- domain/src/main/java/com/bluepig/alarm/domain/usecase/GetAllAlarms.kt
+++ domain/src/main/java/com/bluepig/alarm/domain/usecase/GetAllAlarms.kt
@@ -1,17 +1,27 @@
 package com.bluepig.alarm.domain.usecase
 
 import com.bluepig.alarm.domain.repository.AlarmRepository
+import com.bluepig.alarm.domain.util.hourOfDay
+import com.bluepig.alarm.domain.util.minute
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
+/**
+ * Get all alarms
+ *
+ * 현재 저장된 알람 리스트를 시간으로 정렬해서 가져온다.
+ *
+ * @property _repository
+ * @constructor Create empty Get all alarms
+ */
 class GetAllAlarms @Inject constructor(
     private val _repository: AlarmRepository,
 ) {
-    // TODO: 현재 Alarm.timeInMillis를 기준으로 하기에 시간, 분 기준이 아니라 수정 필요.(시간, 분은 늦어도 날짜가 빠르면 먼저보여주게됨.)
     operator fun invoke() = _repository.getAllAlarmFlow()
         .map { alarmList ->
-            alarmList
-                .sortedBy { it.timeInMillis }
-                .sortedByDescending { it.isActive }
+            alarmList.sortedBy {
+                val calendar = it.getCalendar()
+                (calendar.hourOfDay * 60) + calendar.minute
+            }
         }
 }
