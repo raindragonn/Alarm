e9231e4d27b2522205de7ca98e48c47c6e2f4c9a
raindragonn <raindragonn@gmail.com>
2023-12-01 20:25:00 +0900

    [REFACTOR] MainViewModel 생성

--- app/src/main/java/com/bluepig/alarm/ui/main/MainActivity.kt
+++ app/src/main/java/com/bluepig/alarm/ui/main/MainActivity.kt
@@ -1,35 +1,23 @@
 package com.bluepig.alarm.ui.main
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
 import androidx.media3.common.util.UnstableApi
 import androidx.media3.exoplayer.offline.DownloadService
 import com.bluepig.alarm.databinding.ActivityMainBinding
-import com.bluepig.alarm.domain.usecase.GetAllAlarms
-import com.bluepig.alarm.manager.download.MediaDownloadManager
 import com.bluepig.alarm.notification.NotificationType
 import com.bluepig.alarm.service.MediaDownloadService
 import com.bluepig.alarm.util.PermissionHelper
 import com.bluepig.alarm.util.viewBinding
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.stateIn
-import kotlinx.coroutines.launch
 import timber.log.Timber
-import javax.inject.Inject
 
 @UnstableApi
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private val _binding: ActivityMainBinding by viewBinding(ActivityMainBinding::inflate)
-
-    @Inject
-    lateinit var getAllAlarms: GetAllAlarms
-
-    @Inject
-    lateinit var mediaDownloadManager: MediaDownloadManager
+    private val _vm: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,21 +27,7 @@ class MainActivity : AppCompatActivity() {
         }
         PermissionHelper.checkSystemAlertPermission(this, _binding.root)
         PermissionHelper.checkExactAlarmPermission(this, _binding.root)
-
         startDownloadService()
-
-        lifecycleScope.launch {
-            repeatOnLifecycle(Lifecycle.State.CREATED) {
-                getAllAlarms.invoke()
-                    .stateIn(this)
-                    .collect { list ->
-                        list.filter { it.isActive }
-                            .forEach {
-                                mediaDownloadManager.startDownload(it.file)
-                            }
-                    }
-            }
-        }
     }
 
     private fun startDownloadService() {
@@ -68,8 +42,7 @@ class MainActivity : AppCompatActivity() {
                 MediaDownloadService::class.java,
             )
             Timber.e(it)
-        }.onSuccess {
-            Timber.d("success")
         }
+        _vm.startDownload()
     }
 }
