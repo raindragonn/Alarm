c7ed1f8d7f08119ead144f52dbaadb0da248548d
raindragonn <raindragonn@gmail.com>
2023-12-15 21:28:16 +0900

    [REFACTOR] 알람 선택 다이얼로그 로딩 로직 변경

--- app/src/main/java/com/bluepig/alarm/ui/search/select/MediaSelectBottomSheetDialogFragment.kt
+++ app/src/main/java/com/bluepig/alarm/ui/search/select/MediaSelectBottomSheetDialogFragment.kt
@@ -14,15 +14,18 @@ import com.bluepig.alarm.R
 import com.bluepig.alarm.databinding.DialogFragmentMediaSelectBinding
 import com.bluepig.alarm.domain.entity.alarm.media.AlarmMedia
 import com.bluepig.alarm.domain.result.onFailureWitLoading
+import com.bluepig.alarm.domain.result.onLoading
 import com.bluepig.alarm.manager.player.SongPlayerManager
 import com.bluepig.alarm.ui.edit.AlarmEditFragment
 import com.bluepig.alarm.util.ext.setThumbnail
 import com.bluepig.alarm.util.ext.showErrorToast
+import com.bluepig.alarm.util.ext.viewLifeCycleScope
 import com.bluepig.alarm.util.ext.viewRepeatOnLifeCycle
 import com.bluepig.alarm.util.viewBinding
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -61,32 +64,26 @@ class MediaSelectBottomSheetDialogFragment :
         btnClose.setOnClickListener { findNavController().popBackStack() }
         btnPlay.setOnClickListener { playerManager.playEndPause() }
         btnSelect.setOnClickListener {
-            val alarmMedia = _vm.musicMedia.value.getOrNull()
-            if (alarmMedia == null) showErrorToast(null)
+            viewLifeCycleScope.launch {
+                val alarmMedia = _vm.musicMedia.value.getOrNull()
+                if (alarmMedia == null) {
+                    showErrorToast(null)
+                    return@launch
+                }
 
-            setFragmentResult(
-                AlarmEditFragment.REQUEST_ALARM_MEDIA,
-                bundleOf(AlarmEditFragment.KEY_ALARM_MEDIA to alarmMedia)
-            )
-            findNavController()
-                .popBackStack(R.id.alarmEditFragment, false)
+                setFragmentResult(
+                    AlarmEditFragment.REQUEST_ALARM_MEDIA,
+                    bundleOf(AlarmEditFragment.KEY_ALARM_MEDIA to alarmMedia)
+                )
+                findNavController()
+                    .popBackStack(R.id.alarmEditFragment, false)
+            }
         }
     }
 
-    /**
-     * On playing state change
-     * 현재 재생여부에 따른 콜백, 로딩이 끝난 다음에 불린다.
-     * TODO: 플레이 여부 외에 현재 로딩 여부를 따로 정의하는것도 고려할만함
-     * @param isPlaying 현재 플레이 여부를 나타낸다.
-     */
     private fun onPlayingStateChange(isPlaying: Boolean) {
         with(_binding) {
-            if (pbLoading.isVisible) {
-                pbLoading.isVisible = false
-                group.isVisible = true
-            }
-
-            btnPlay.text = if (isPlaying) "멈춤" else "재생"
+            btnPlay.text = if (isPlaying) getString(R.string.pause) else getString(R.string.play)
         }
     }
 
@@ -95,14 +92,18 @@ class MediaSelectBottomSheetDialogFragment :
             _vm.musicMedia
                 .stateIn(this)
                 .collect { result ->
-                    result.onSuccess {
-                        bindMedia(it)
-                        playerManager.play(it)
-                    }.onFailureWitLoading {
-                        showErrorToast(it) {
-                            findNavController().popBackStack()
+                    result
+                        .onSuccess {
+                            bindMedia(it)
+                            playerManager.play(it)
+                        }.onFailureWitLoading {
+                            showErrorToast(it) {
+                                findNavController().popBackStack()
+                            }
+                        }.onLoading {
+                            _binding.pbLoading.isVisible = it
+                            _binding.groupContent.isVisible = it.not()
                         }
-                    }
                 }
         }
     }
